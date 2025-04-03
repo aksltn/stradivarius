a47417f62c1b44daa216d637e34c6a43cb85e633
aks264 <aks264@cornell.edu>
2025-04-03 13:01:11 -0400

    Iterated by loading single image

--- p5_script.js
+++ p5_script.js
@@ -19,6 +19,13 @@ const images = {
 const canvasWidth = gridWidth * pixelSize;
 const canvasHeight = gridHeight * pixelSize;
 
+function preload() {
+    red_img = loadImage('assets/red/red-1.jpg');
+    blue_img = loadImage('assets/blue/blue-1.jpg');
+    white_img = loadImage('assets/white/white-1.jpg');
+    multicolor_img = loadImage('assets/multicolor/multicolor-1.jpg');
+}
+
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
     noLoop();
@@ -31,13 +38,27 @@ function draw() {
         for (let x = 0; x < gridWidth; x++) {
             let data = noise(x * noiseScale, y * noiseScale);
 
-            console.log(data);
-            
-            fill(assign(data));
-            
-            noStroke();
+            // console.log(data);
 
+            let src = assign_img(data);
+
+            image(
+                img=src[0], 
+                dx=x * pixelSize, 
+                dy=y * pixelSize,
+                dw=pixelSize,
+                dh=pixelSize,
+                sx=src[1],
+                sy=src[2],
+                sw=pixelSize,
+                sh=pixelSize
+            )
+            
+            /*
+            fill(assign(data));
+            noStroke();
             rect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+            */
         }
     }
 }
@@ -57,6 +78,27 @@ function assign(data) {
     }
 
     return col;
+}
+
+function assign_img(data) {
+
+    imageMode(CORNER);
+
+    let output;
+
+    if (data < 0.15) {
+        output = [blue_img, floor(random(blue_img.width - pixelSize)), floor(random(blue_img.height - pixelSize))];
+    } else if (data < 0.3) {
+        output = [red_img, floor(random(red_img.width - pixelSize)), floor(random(red_img.height - pixelSize))];
+    } else if (data < 0.45) {
+        output = [white_img, floor(random(white_img.width - pixelSize)), floor(random(white_img.height - pixelSize))];
+    } else {
+        output = [multicolor_img, floor(random(multicolor_img.width - pixelSize)), floor(random(multicolor_img.height - pixelSize))];
+    }
+
+    // console.log("OUTPUT", output);
+
+    return output;
 }
 
 setup();
